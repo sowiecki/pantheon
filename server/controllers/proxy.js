56bf8ffda46e1d025270b96515c835b42a16916d
sowiecki <sean.owiecki@gmail.com>
2016-08-24 20:46:41 -0500

    Fix proxy bug

--- server/controllers/proxy.js
+++ server/controllers/proxy.js
@@ -2,7 +2,8 @@
 /* globals console */
 import WebSocket from 'ws';
 
-import { buzzerController, deadboltController } from './buzzer';
+import { buzzerController } from './buzzer';
+import { deadboltController } from './deadbolt';
 import { config } from '../environment';
 import { WEBSOCKET_PROTOCOL,
          WEBSOCKET_RECONNECT_INTERVAL,
