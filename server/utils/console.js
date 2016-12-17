bbda8f4a7d15e66b35b62d2156ec4051cc91fe93
sowiecki <nase00@gmail.com>
2016-12-17 16:11:38 -0600

    Add basic Winston logging

--- server/utils/console.js
+++ server/utils/console.js
@@ -1,9 +1,21 @@
 /* eslint no-console:0, import/prefer-default-export:0 */
+import winston from 'winston';
 import moment from 'moment';
 import colors from 'colors';
+
+const logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)(),
+    new (winston.transports.File)({
+      filename: 'events.log',
+      timestamp: false,
+      maxsize: 100000000
+    })
+  ]
+});
 
 export const logActionType = (type) => {
   const time = moment().format('M/D/YY hh:mm:ss A');
 
-  console.log(`${colors.grey(time)} | ${colors.yellow(type)}`);
+  logger.log('info', `${colors.grey(time)} | ${colors.yellow(type)}`);
 };
