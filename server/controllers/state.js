5fe6e8f3e97cb8804d4decea4d340e410b38067d
sowiecki <nase00@gmail.com>
2017-01-30 00:08:49 -0600

    Remove unused custom state controller update method

--- server/controllers/state.js
+++ server/controllers/state.js
@@ -1,5 +1,4 @@
 import { EMIT_CUSTOM_STATE_REGISTER } from 'ducks/devices';
-import { EMIT_CUSTOM_STATE_UPDATE } from 'ducks/occurrences';
 import { DEVICE_TYPES } from 'constants';
 import store from 'store';
 
@@ -9,13 +8,6 @@ const stateController = () => ({
       type: EMIT_CUSTOM_STATE_REGISTER,
       deviceType
     }));
-  },
-
-  update(newCustomState) {
-    store.dispatch({
-      type: EMIT_CUSTOM_STATE_UPDATE,
-      newCustomState
-    });
   }
 });
 
