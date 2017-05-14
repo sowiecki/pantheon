d1407cf24ca296e3ddf9984348db8986456b9875
sowiecki <nase00@gmail.com>
2017-05-13 21:23:23 -0500

    Lower spotify token refresh

--- server/constants/services.js
+++ server/constants/services.js
@@ -17,5 +17,5 @@ export const SPOTIFY_PERMISSION_SCOPES = [
   'streaming',
   'user-top-read'
 ];
-export const SPOTIFY_TOKEN_REFRESH_INVERVAL = 3540000; // 59 minutes
+export const SPOTIFY_TOKEN_REFRESH_INVERVAL = 1140000; // 19 minutes
 export const SPOTIFY_SYNC_STATE_TIMEOUT = 250;
