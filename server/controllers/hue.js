9c2e04e616ecb7b569f2de266be060e18fe46bd4
Sean <me@seanowiecki.com>
2020-05-11 17:53:06 -0500

    Update Hue API.

--- server/controllers/hue.js
+++ server/controllers/hue.js
@@ -1,32 +1,35 @@
-import hue from 'node-hue-api';
+import hue from "node-hue-api";
 
-import { ENV } from 'config';
-import { EMIT_REGISTER_BRIDGE } from 'ducks/devices';
-import store from 'store';
-import { errorNoUserIDFound } from 'utils';
+import { ENV } from "config";
+import { EMIT_REGISTER_BRIDGE } from "ducks/devices";
+import store from "store";
+import { errorNoUserIDFound } from "utils";
 
 const hueController = {
-  displayName: 'Hue Controller',
+  displayName: "Hue Controller",
 
   shouldInit: () => !!ENV.hueUserIDs,
 
   initialize() {
-    hue.nupnpSearch().then((bridges) => {
-      bridges.map((bridge) => {
-        const userID = ENV.hueUserIDs[bridge.ipaddress];
+    hue
+      .upnpSearch(2000)
+      .then((bridges) => {
+        bridges.map((bridge) => {
+          const userID = ENV.hueUserIDs[bridge.ipaddress];
+          if (!userID) {
+            return errorNoUserIDFound(bridge.ipaddress);
+          }
+          console.log("Registered Hue bridge", bridge.ipaddress);
 
-        if (!userID) {
-          return errorNoUserIDFound(bridge.ipaddress);
-        }
-
-        return store.dispatch({
-          type: EMIT_REGISTER_BRIDGE,
-          bridge,
-          userID
+          return store.dispatch({
+            type: EMIT_REGISTER_BRIDGE,
+            bridge,
+            userID,
+          });
         });
-      });
-    }).done();
-  }
+      })
+      .done();
+  },
 };
 
 export default hueController;
